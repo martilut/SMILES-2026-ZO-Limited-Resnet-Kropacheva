405d8b02509a3906d6ee1dfaf079d591c1e1393f
martilut <jeykolton@gmail.com>
2026-05-10 20:49:05 +0300

    add config

---
+++ config.py
@@ -0,0 +1,8 @@
+SEED = 0
+SUBSET_SIZE = None # None = full dataset
+HEAD_INIT = "prior" # "kaiming", "xavier", "orthogonal", "small_random", "prior"
+OPTIMIZATION_MODE = "fc_bn_all"
+BATCH_SIZE = 32
+N_BATCHES = 32
+SPSA_K = 4
+FREEZE = True
