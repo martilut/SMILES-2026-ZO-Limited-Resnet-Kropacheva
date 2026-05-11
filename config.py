335715ed209e18e6eb58ebbf9c0542b855d02146
martilut <jeykolton@gmail.com>
2026-05-11 21:42:37 +0300

    update config

--- config.py
+++ config.py
@@ -1,8 +1,16 @@
 SEED = 0
 SUBSET_SIZE = None # None = full dataset
-HEAD_INIT = "prior" # "kaiming", "xavier", "orthogonal", "small_random", "prior"
-OPTIMIZATION_MODE = "fc_bn_all"
-BATCH_SIZE = 32
-N_BATCHES = 32
+HEAD_INIT = "kaiming" # "kaiming", "xavier", "orthogonal", "small_random", "prior"
+OPTIMIZATION_MODE = "fc"
+BATCH_SIZE = 128
+N_BATCHES = 64
 SPSA_K = 4
-FREEZE = True
+FREEZE = False
+MOVING_AVERAGE_COEFF = 0.5
+UPDATE_RULE = "momentum"   # "momentum", "adam"
+ADAM_BETA1 = 0.9
+ADAM_BETA2 = 0.999
+ADAM_EPS = 1e-8
+LR = 1e-3
+EPS = 1e-3
+PERTURBATION_MODE = "rademacher" # "gaussian", "rademacher", "uniform"
